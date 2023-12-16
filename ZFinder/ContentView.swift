a91ec9ea6387011d2c7585d33569fa3f9bd9474d
shrey <shrey.gangwar21@gmail.com>
2023-12-15 22:51:15 -0800

    Initial Commit

---
+++ ZFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZFinder
+//
+//  Created by Shrey Gangwar on 12/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZFinder/ZFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZFinderApp.swift
+//  ZFinder
+//
+//  Created by Shrey Gangwar on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ZFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
